988ceb3b5aa370e47c13088a3338aa6da4ff09d7
Elmercito16 <121210214+Elmercito16@users.noreply.github.com>
2025-06-11 10:27:04 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -1,11 +1,16 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+NOTICIA DE LA CATEGORÍA ECONOMÍA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Inflación en Perú cae por tercer mes consecutivo",
+    "autor" => "El Comercio",
+    "resumen" => "El Instituto Nacional de Estadística e Informática (INEI) informó que la inflación nacional disminuyó un 0.2% en mayo, marcando el tercer mes consecutivo de descenso. Esta baja se debe principalmente a la reducción en los precios de alimentos y transporte.",
 ];
+
+// Mostrar contenido (opcional)
+echo "<h1>" . $economia['titulo'] . "</h1>";
+echo "<p><strong>Autor:</strong> " . $economia['autor'] . "</p>";
+echo "<p>" . $economia['resumen'] . "</p>";
 ?>
