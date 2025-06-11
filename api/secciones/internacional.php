a0ee7cfce86fcee72f3bad28919c926b2dee334c
Elmercito16 <121210214+Elmercito16@users.noreply.github.com>
2025-06-11 10:27:37 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -3,9 +3,9 @@
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
-$internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+$cultura = [
+    "titulo" => "La UNESCO declara a la cerámica de Chulucanas como Patrimonio Cultural Inmaterial",
+    "autor" => "Agencia Andina",
+    "resumen" => "La cerámica tradicional de Chulucanas, reconocida por su estilo único y técnicas ancestrales, ha sido incluida en la lista de Patrimonio Cultural Inmaterial de la Humanidad por la UNESCO. Este reconocimiento busca preservar el legado artístico y cultural del norte peruano.",
 ];
 ?>
